5494b5e4eec2dda7d15ee6efff68004d8983eb84
Daniel Antonio <daniel23.da74@gmail.com>
2022-06-27 03:24:59 -0500

    creando auth y user

---
+++ backend/src/services/auth.js
@@ -0,0 +1,74 @@
+const User = require("./users");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const { secret } = require("./../config");
+
+const serviceUser = new User();
+class Auth {
+  async login(payload) {
+    const { email, password } = payload;
+    const user = await serviceUser.getByEmail(email);
+    // console.log(user);
+
+    if (user && this.compare(password, user.password)) {
+      delete user.password;
+      const token = this.createToken(user);
+      return {
+        logged: true,
+        data: user,
+        token,
+      };
+    }
+    return {
+      logged: false,
+      message: "Credentials are not valid",
+    };
+  }
+
+  async signUp(payload) {
+    payload.password = await this.encrypt(payload.password);
+    const user = await serviceUser.create(payload);
+
+    if (user) {
+      delete user.password;
+      const token = this.createToken(user);
+      return {
+        logged: true,
+        user,
+        token,
+      };
+    }
+  }
+
+  validate(token) {
+    try {
+      const data = jwt.verify(token, secret);
+      return {
+        success: true,
+        data,
+      };
+    } catch ({ message }) {
+      return {
+        success: false,
+        message,
+      };
+    }
+  }
+
+  createToken(payload) {
+    return jwt.sign(payload, secret, {
+      expiresIn: "7d",
+    });
+  }
+
+  async encrypt(password) {
+    const salt = await bcrypt.genSalt();
+    return await bcrypt.hash(password, salt);
+  }
+
+  async compare(password, hash) {
+    return await bcrypt.compare(password, hash);
+  }
+}
+
+module.exports = Auth;
